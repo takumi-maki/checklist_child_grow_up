f4cd6328aff10d258e5d7b0b307abc9ad30b6a49
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2022-10-22 14:27:56 +0900

    loading_button resetAfterDuration: trueに変更

--- lib/utils/loading/loading_button.dart
+++ lib/utils/loading/loading_button.dart
@@ -27,6 +27,7 @@ class _LoadingButtonState extends State<LoadingButton> {
         onPressed: () async {
           await widget.onPressed();
         },
+        resetAfterDuration: true,
         borderRadius: 20,
         height: 36,
         width: 150,
