036c954a2aa6a8c0ca17fed459173490849b3790
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2022-10-23 10:31:31 +0900

    縦画面 固定にする

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:checklist_child_grow_up/firebase_options.dart';
 import 'package:checklist_child_grow_up/view/start_up/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
