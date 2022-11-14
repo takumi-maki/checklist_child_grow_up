a3db1fc34dca46af952725a8c3b33e17b1131dab
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2022-11-14 10:57:55 +0900

    不要なimportを削除

--- lib/utils/function_utils.dart
+++ lib/utils/function_utils.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
-import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 class FunctionUtils {
   static Future<List<dynamic>> getCheckListItems() async {
