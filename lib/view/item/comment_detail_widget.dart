1716c55a7048b8631695cad6f74f5b3072b0deab
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2022-12-04 16:20:54 +0900

    コメントの画像のwidgetを別ファイルに切り離す

--- lib/view/item/comment_detail_widget.dart
+++ lib/view/item/comment_detail_widget.dart
@@ -1,18 +1,14 @@
-import 'dart:typed_data';
-
 import 'package:checklist_child_grow_up/utils/firestore/accounts.dart';
 import 'package:checklist_child_grow_up/utils/firestore/comments.dart';
-import 'package:checklist_child_grow_up/utils/loading/loading_gesture_detector.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 import '../../model/comment.dart';
 import '../../utils/firestore/authentications.dart';
 import '../../utils/widget_utils.dart';
-import 'image_preview_widget.dart';
+import 'comment_image_widget.dart';
 
 class CommentDetailWidget extends StatefulWidget {
   final String roomId;
@@ -77,14 +73,7 @@ class _CommentDetailWidgetState extends State<CommentDetailWidget> {
     return thisCommentCreatedAt.difference(prevCommentCreatedAt).inDays == 0
         && thisCommentCreatedAt.day == prevCommentCreatedAt.day;
   }
-  Future<Uint8List> convertImagePathToUint8List(String imagePath) async {
-    return (await NetworkAssetBundle(Uri.parse(imagePath)).load(imagePath)).buffer.asUint8List();
-  }
-  void showPreviewImage(Uint8List image) {
-    showDialog(context: context, builder: (context) {
-      return ImagePreviewWidget(image: image);
-    });
-  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -114,66 +103,7 @@ class _CommentDetailWidgetState extends State<CommentDetailWidget> {
             ? const SizedBox()
             : Padding(
               padding: const EdgeInsets.only(bottom: 8.0),
-              child: LoadingGestureDetector(
-                onTap: () async {
-                  Uint8List image =  await convertImagePathToUint8List(widget.comment.imagePath!);
-                  if (!mounted) return;
-                  return showPreviewImage(image);
-                },
-                child: Container(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width * 0.50,
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12.0),
-                      child: Image.network(
-                        widget.comment.imagePath!,
-                        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                          if (loadingProgress == null) return child;
-                          return Container(
-                            width: MediaQuery.of(context).size.width * 0.50,
-                            height: MediaQuery.of(context).size.width * 0.40,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            child: Center(
-                              child: SizedBox(
-                                height: 20.0,
-                                width: 20.0,
-                                child: CircularProgressIndicator(
-                                  value: loadingProgress.expectedTotalBytes != null
-                                    ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                                    : null,
-                                  valueColor: const AlwaysStoppedAnimation(Colors.amber),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                        errorBuilder: (BuildContext context, Object object, StackTrace? stackTrace) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width * 0.50,
-                            height: MediaQuery.of(context).size.width * 0.40,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const Icon(Icons.error_outline),
-                                  Text('画像の取得に失敗しました', style: Theme.of(context).textTheme.bodySmall)
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      )
-                    ),
-                ),
-              ),
+              child: CommentImageWidget(comment: widget.comment),
             ),
           widget.comment.text == null
             ? const SizedBox()
