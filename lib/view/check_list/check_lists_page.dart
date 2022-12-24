9c3f5009a9303778c9965015c7827ab0291ed0a4
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2022-12-24 14:25:18 +0900

    まとめて達成機能を実装

--- lib/view/check_list/check_lists_page.dart
+++ lib/view/check_list/check_lists_page.dart
@@ -1,6 +1,7 @@
+import 'package:checklist_child_grow_up/utils/firestore/rooms.dart';
+import 'package:checklist_child_grow_up/view/check_list/bulk_achieve_check_list_dialog.dart';
 import 'package:checklist_child_grow_up/view/check_list/check_lists_app_bar_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:checklist_child_grow_up/utils/firestore/rooms.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,12 +10,9 @@ import '../../utils/firestore/authentications.dart';
 import 'check_list_card_detail_widget.dart';
 
 class CheckListsPageWidget extends StatefulWidget {
-  const CheckListsPageWidget({
-    Key? key,
-    required this.childName,
-    this.ageMonths,
-    required this.roomId
-  }) : super(key: key);
+  const CheckListsPageWidget(
+      {Key? key, required this.childName, this.ageMonths, required this.roomId})
+      : super(key: key);
 
   final String childName;
   final int? ageMonths;
@@ -28,7 +26,8 @@ class _CheckListsPageWidgetState extends State<CheckListsPageWidget> {
   final User currentFirebaseUser = AuthenticationFirestore.currentFirebaseUser!;
 
   String getChildName(AsyncSnapshot<DocumentSnapshot<Object?>> roomSnapshot) {
-    Map<String, dynamic> data = roomSnapshot.data!.data() as Map<String, dynamic>;
+    Map<String, dynamic> data =
+        roomSnapshot.data!.data() as Map<String, dynamic>;
     return data['child_name'];
   }
 
@@ -36,19 +35,17 @@ class _CheckListsPageWidgetState extends State<CheckListsPageWidget> {
     List docItems = checkListSnapshot['items'];
     final List<Item> items = docItems.map((item) {
       return Item(
-        id: item['id'],
-        month: item['month'],
-        isAchieved: item['is_achieved'],
-        content: item['content'],
-        achievedTime: item['achieved_time']
-      );
+          id: item['id'],
+          month: item['month'],
+          isAchieved: item['is_achieved'],
+          content: item['content'],
+          achievedTime: item['achieved_time']);
     }).toList();
     CheckList checkList = CheckList(
-      id: checkListSnapshot['id'],
-      roomId: checkListSnapshot['room_id'],
-      type: checkListSnapshot['type'],
-      items: items
-    );
+        id: checkListSnapshot['id'],
+        roomId: checkListSnapshot['room_id'],
+        type: checkListSnapshot['type'],
+        items: items);
     return checkList;
   }
 
@@ -57,79 +54,129 @@ class _CheckListsPageWidgetState extends State<CheckListsPageWidget> {
     for (final comment in commentSnapshot) {
       Map<String, dynamic> data = comment.data() as Map<String, dynamic>;
       List commentReadAccountIds = data['read_account_ids'];
-      if(!commentReadAccountIds.contains(currentFirebaseUser.uid)) {
-        unreadCommentsCount ++;
+      if (!commentReadAccountIds.contains(currentFirebaseUser.uid)) {
+        unreadCommentsCount++;
       }
     }
     return unreadCommentsCount;
   }
 
+  double getCheckListItemsWidgetHeight() {
+    double checkListItemsWidgetHeight;
+    const appBarWidgetHeight = 138.0;
+    const bulkAchievementWidgetHeight = 40.0;
+    checkListItemsWidgetHeight = MediaQuery.of(context).size.height -
+        appBarWidgetHeight -
+        bulkAchievementWidgetHeight -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
+    return checkListItemsWidgetHeight;
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: CheckList.tabBarList.length,
-      child: Scaffold(
-        appBar: CheckListsAppBarWidget(
-          ageMonths: widget.ageMonths,
-          roomId: widget.roomId
-        ),
-        body: SafeArea(
-          child: StreamBuilder<QuerySnapshot>(
-              stream: RoomFirestore.rooms.doc(widget.roomId)
-                  .collection('check_lists').orderBy('type', descending: false)
-                  .snapshots(),
-              builder: (context, checkListSnapshot) {
-                if(!checkListSnapshot.hasData || checkListSnapshot.data!.docs.length < CheckList.tabBarList.length) {
+        length: CheckList.tabBarList.length,
+        child: Scaffold(
+          appBar: CheckListsAppBarWidget(
+              ageMonths: widget.ageMonths, roomId: widget.roomId),
+          body: SafeArea(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: RoomFirestore.rooms
+                    .doc(widget.roomId)
+                    .collection('check_lists')
+                    .orderBy('type', descending: false)
+                    .snapshots(),
+                builder: (context, checkListSnapshot) {
+                  if (!checkListSnapshot.hasData ||
+                      checkListSnapshot.data!.docs.length <
+                          CheckList.tabBarList.length) {
+                    return TabBarView(
+                        children: CheckList.tabBarList.map((e) {
+                      return const SizedBox();
+                    }).toList());
+                  }
                   return TabBarView(
-                      children: CheckList.tabBarList.map((e) {
-                        return const SizedBox();
-                      }).toList()
-                  );
-                }
-                return TabBarView(
-                    children: checkListSnapshot.data!.docs.map((doc) {
-                      CheckList checkList = getCheckList(doc);
-                      return ListView.builder(
-                          itemCount: checkList.items.length,
-                          itemBuilder: (context, index) {
-                            final Item item = checkList.items[index];
-                            return StreamBuilder<QuerySnapshot>(
-                                stream: RoomFirestore.rooms.doc(checkList.roomId)
-                                    .collection('check_lists').doc(checkList.id)
-                                    .collection('comments').orderBy('created_time', descending: false)
-                                    .where('item_id', isEqualTo: item.id)
-                                    .snapshots(),
-                                builder: (context, commentSnapshot) {
-                                  if (!commentSnapshot.hasData) {
-                                    return const SizedBox(
-                                      height: 64.0,
-                                      width: double.infinity,
-                                    );
-                                  }
-                                  if (commentSnapshot.data!.docs.isNotEmpty) {
-                                    final unreadCommentsCount = countUnreadComments(commentSnapshot.data!.docs);
-                                    return CheckListCardDetailWidget(
-                                      checkList: checkList,
-                                      item: item,
-                                      hasComments: true,
-                                      unreadCommentsCount: unreadCommentsCount,
-                                    );
-                                  }
-                                  return CheckListCardDetailWidget(
-                                    checkList: checkList,
-                                    item: item,
-                                    hasComments: false,
-                                  );
-                                }
-                            );
-                          }
-                      );
-                    }).toList()
-                );
-              }
+                      children: checkListSnapshot.data!.docs.map((doc) {
+                    CheckList checkList = getCheckList(doc);
+                    return Column(
+                      children: [
+                        SizedBox(
+                            height: 40.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: OutlinedButton.icon(
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        barrierDismissible: false,
+                                        builder: (context) {
+                                          return BulkAchieveCheckListDialog(
+                                              checkList: checkList);
+                                        });
+                                  },
+                                  style: OutlinedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0)
+                                    )
+                                  ),
+                                  icon: const Icon(
+                                    Icons.edit_note,
+                                    color: Colors.black87,
+                                  ),
+                                  label: Text('まとめて達成',
+                                      style: Theme.of(context)
+                                          .textTheme.subtitle2)),
+                            )),
+                        SizedBox(
+                          height: getCheckListItemsWidgetHeight(),
+                          child: ListView.builder(
+                              itemCount: checkList.items.length,
+                              itemBuilder: (context, index) {
+                                final Item item = checkList.items[index];
+                                return StreamBuilder<QuerySnapshot>(
+                                    stream: RoomFirestore.rooms
+                                        .doc(checkList.roomId)
+                                        .collection('check_lists')
+                                        .doc(checkList.id)
+                                        .collection('comments')
+                                        .orderBy('created_time',
+                                            descending: false)
+                                        .where('item_id', isEqualTo: item.id)
+                                        .snapshots(),
+                                    builder: (context, commentSnapshot) {
+                                      if (!commentSnapshot.hasData) {
+                                        return const SizedBox(
+                                          height: 64.0,
+                                          width: double.infinity,
+                                        );
+                                      }
+                                      if (commentSnapshot
+                                          .data!.docs.isNotEmpty) {
+                                        final unreadCommentsCount =
+                                            countUnreadComments(
+                                                commentSnapshot.data!.docs);
+                                        return CheckListCardDetailWidget(
+                                          checkList: checkList,
+                                          item: item,
+                                          hasComments: true,
+                                          unreadCommentsCount:
+                                              unreadCommentsCount,
+                                        );
+                                      }
+                                      return CheckListCardDetailWidget(
+                                        checkList: checkList,
+                                        item: item,
+                                        hasComments: false,
+                                      );
+                                    });
+                              }),
+                        ),
+                      ],
+                    );
+                  }).toList());
+                }),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
