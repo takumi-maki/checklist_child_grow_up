59f7a1293c7859672d4cad93b6a004a5dce2d07d
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2024-03-03 06:46:05 +0900

    広告を読み込めない時の応急処置

--- lib/view/about/about_app_menus_page.dart
+++ lib/view/about/about_app_menus_page.dart
@@ -1,11 +1,11 @@
 import 'package:checklist_child_grow_up/view/banner/ad_banner_widget.dart';
 import 'package:checklist_child_grow_up/view/widget_utils/app_bar/app_bar_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import '../../model/tile.dart';
 import '../../utils/advertisement/advertisement_rewarded.dart';
 import '../../utils/launch_url.dart';
+import '../widget_utils/snack_bar/error_snack_bar_widget.dart';
 
 class AboutAppMenusPage extends StatefulWidget {
   const AboutAppMenusPage({Key? key}) : super(key: key);
@@ -31,31 +31,33 @@ class _AboutAppMenusPageState extends State<AboutAppMenusPage> {
     rewardedAd.dispose();
   }
 
-  List<Tile> generateAboutAppMenus () {
+  List<Tile> generateAboutAppMenus() {
     final launchUrl = LaunchUrl();
     return [
       Tile(
           leading: const Icon(Icons.volunteer_activism),
           title: const Text('広告を観てアプリを支援'),
-          onTap: () => {
-            rewardedAd.showAd()
-          }
-      ),
+          onTap: () {
+            if (rewardedAd.rewardedAd == null) {
+              final validationErrorSnackBar =
+                  ErrorSnackBar(context, title: '広告が読み込めませんでした');
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(validationErrorSnackBar);
+            }
+            rewardedAd.showAd();
+          }),
       Tile(
-        leading: const Icon(Icons.security),
-        title: const Text('プライバシーポリシー'),
-        onTap: () => launchUrl.privacyPolicy()
-      ),
+          leading: const Icon(Icons.security),
+          title: const Text('プライバシーポリシー'),
+          onTap: () => launchUrl.privacyPolicy()),
       Tile(
-        leading: const Icon(Icons.description),
-        title: const Text('利用規約'),
-        onTap: () => launchUrl.termsOfService()
-      ),
+          leading: const Icon(Icons.description),
+          title: const Text('利用規約'),
+          onTap: () => launchUrl.termsOfService()),
       Tile(
-        leading: const Icon(Icons.contact_mail),
-        title: const Text('お問い合わせ'),
-        onTap: () => launchUrl.contactForm()
-      ),
+          leading: const Icon(Icons.contact_mail),
+          title: const Text('お問い合わせ'),
+          onTap: () => launchUrl.contactForm()),
     ];
   }
 
@@ -65,23 +67,26 @@ class _AboutAppMenusPageState extends State<AboutAppMenusPage> {
       appBar: const AppBarWidget(title: 'アプリについて'),
       body: SafeArea(
         child: ListView.builder(
-          itemCount: aboutAppMenus.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: aboutAppMenus[index].leading,
-                  title: aboutAppMenus[index].title,
-                  trailing: const Icon(Icons.arrow_forward_ios),
-                  iconColor: aboutAppMenus[index].isErrorText ? Theme.of(context).errorColor : Colors.black87,
-                  textColor: aboutAppMenus[index].isErrorText ? Theme.of(context).errorColor : Colors.black87,
-                  onTap: aboutAppMenus[index].onTap,
+            itemCount: aboutAppMenus.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: aboutAppMenus[index].leading,
+                    title: aboutAppMenus[index].title,
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    iconColor: aboutAppMenus[index].isErrorText
+                        ? Theme.of(context).colorScheme.error
+                        : Colors.black87,
+                    textColor: aboutAppMenus[index].isErrorText
+                        ? Theme.of(context).colorScheme.error
+                        : Colors.black87,
+                    onTap: aboutAppMenus[index].onTap,
+                  ),
                 ),
-              ),
-            );
-          }
-        ),
+              );
+            }),
       ),
       bottomNavigationBar: const AdBannerWidget(),
     );
