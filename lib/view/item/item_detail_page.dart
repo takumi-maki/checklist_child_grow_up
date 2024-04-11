eee5b5bb0eb277c8cab9df7a54790e74a3c9b29c
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2024-04-12 08:33:30 +0900

    コメントWidgetのUI修正 バナー広告の高さ分調整

--- lib/view/item/item_detail_page.dart
+++ lib/view/item/item_detail_page.dart
@@ -9,13 +9,15 @@ import '../../utils/firestore/rooms.dart';
 import '../banner/ad_banner_widget.dart';
 import '../widget_utils/app_bar/app_bar_widget.dart';
 import '../widget_utils/loading/circular_progress_indicator_widget.dart';
-import 'achievement_button_widget.dart';
 import 'achieved_time_widget.dart';
+import 'achievement_button_widget.dart';
 import 'comment_detail_widget.dart';
 import 'text_input_widget.dart';
 
 class ItemDetailPage extends StatefulWidget {
-  const ItemDetailPage({Key? key, required this.checkList, required this.itemId}) : super(key: key);
+  const ItemDetailPage(
+      {Key? key, required this.checkList, required this.itemId})
+      : super(key: key);
 
   final CheckList checkList;
   final String itemId;
@@ -39,11 +41,13 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
     double itemDetailWidgetHeight;
     final appBarWidgetHeight = AppBar().preferredSize.height;
     const double textInputWidgetHeight = 66.0;
-    itemDetailWidgetHeight = MediaQuery.of(context).size.height
-      - appBarWidgetHeight
-      - textInputWidgetHeight
-      - MediaQuery.of(context).padding.top
-      - MediaQuery.of(context).padding.bottom;
+    const double adBannerWidgetHeight = 64.0;
+    itemDetailWidgetHeight = MediaQuery.of(context).size.height -
+        appBarWidgetHeight -
+        textInputWidgetHeight -
+        adBannerWidgetHeight -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
     return itemDetailWidgetHeight;
   }
 
@@ -55,27 +59,23 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
 
   Item getItem(Map<String, dynamic> checkList) {
     List checkListItems = checkList['items'];
-    final item = checkListItems.firstWhere((item){
+    final item = checkListItems.firstWhere((item) {
       return item['id'] == widget.itemId;
     });
     return Item(
-      id: item['id'],
-      month: item['month'],
-      isAchieved: item['is_achieved'],
-      content: item['content'],
-      achievedTime: item['achieved_time']
-    );
+        id: item['id'],
+        month: item['month'],
+        isAchieved: item['is_achieved'],
+        content: item['content'],
+        achievedTime: item['achieved_time']);
   }
 
-  Timestamp? getPrevCommentCreatedTime(int index, List<QueryDocumentSnapshot<Object?>> commentDocs) {
-    final Map<String, dynamic>? prevComment = (
-      index > 0
+  Timestamp? getPrevCommentCreatedTime(
+      int index, List<QueryDocumentSnapshot<Object?>> commentDocs) {
+    final Map<String, dynamic>? prevComment = (index > 0
         ? commentDocs[index - 1].data()
-        : null
-    ) as Map<String, dynamic>?;
-    return prevComment != null
-      ? prevComment['created_time']
-      : null;
+        : null) as Map<String, dynamic>?;
+    return prevComment != null ? prevComment['created_time'] : null;
   }
 
   @override
@@ -86,108 +86,129 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
         appBar: const AppBarWidget(title: 'アイテム詳細'),
         body: SafeArea(
           child: StreamBuilder<DocumentSnapshot>(
-              stream: RoomFirestore.rooms.doc(widget.checkList.roomId)
-                .collection('check_lists').doc(widget.checkList.id)
-                .snapshots(),
-            builder: (context, checkListSnapshot) {
-              if (!checkListSnapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicatorWidget()
-                );
-              }
-              final item = getItem(checkListSnapshot.data!.data() as Map<String, dynamic>);
-              return Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: SizedBox(
-                        height: getItemDetailWidgetHeight(),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Center(child: Image.asset(itemImagePath, height: 80)),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 18.0, top: 10.0, right: 18.0),
-                              child: Text(item.content,
-                                style: Theme.of(context).textTheme.titleLarge,
-                                textAlign: TextAlign.center,
+              stream: RoomFirestore.rooms
+                  .doc(widget.checkList.roomId)
+                  .collection('check_lists')
+                  .doc(widget.checkList.id)
+                  .snapshots(),
+              builder: (context, checkListSnapshot) {
+                if (!checkListSnapshot.hasData) {
+                  return const Center(child: CircularProgressIndicatorWidget());
+                }
+                final item = getItem(
+                    checkListSnapshot.data!.data() as Map<String, dynamic>);
+                return Column(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: SizedBox(
+                          height: getItemDetailWidgetHeight(),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Center(
+                                    child:
+                                        Image.asset(itemImagePath, height: 80)),
                               ),
-                            ),
-                            item.achievedTime == null
-                              ? const SizedBox()
-                              : Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                                child: AchievedTimeWidget(checkList: widget.checkList, item: item),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 18.0, top: 10.0, right: 18.0),
+                                child: Text(
+                                  item.content,
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
-                            AchievementButtonWidget(checkList: widget.checkList, item: item),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                              child: Divider()
-                            ),
-                            Expanded(
-                              child: StreamBuilder<QuerySnapshot>(
-                              stream: RoomFirestore.rooms.doc(widget.checkList.roomId)
-                                .collection('check_lists').doc(widget.checkList.id)
-                                .collection('comments').orderBy('created_time', descending: false)
-                                .where('item_id', isEqualTo: item.id)
-                                .snapshots(),
-                              builder: (context, commentSnapshot) {
-                                if (!commentSnapshot.hasData) {
-                                  return const CircularProgressIndicatorWidget();
-
-                                }
-                                List<QueryDocumentSnapshot<Object?>> commentDocs = commentSnapshot.data!.docs;
-                                if (commentDocs.isEmpty) {
-                                  return const Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 20.0),
-                                    child: Text('コメントはまだありません')
-                                  );
-                                }
-                                return ListView.builder(
-                                  controller: scrollController,
-                                  itemCount: commentDocs.length,
-                                  itemBuilder: (context, index) {
-                                    Timestamp? prevCommentCreatedTime = getPrevCommentCreatedTime(index, commentDocs);
-                                    Map<String, dynamic> data = commentDocs[index].data() as Map<String, dynamic>;
-                                    Comment comment = Comment(
-                                      id: data['id'],
-                                      text: data['text'],
-                                      imagePath: data['image_path'],
-                                      itemId: data['item_id'],
-                                      postedAccountId: data['posted_account_id'],
-                                      readAccountIds: data['read_account_ids'],
-                                      createdTime: data['created_time']
-                                    );
-                                    return CommentDetailWidget(
-                                      roomId: widget.checkList.roomId,
-                                      checkListId: widget.checkList.id,
-                                      comment: comment,
-                                      prevCommentCreatedTime: prevCommentCreatedTime,
-                                      isMine: comment.postedAccountId == currentFirebaseUser.uid
-                                    );
-                                  }
-                                );
-                              }
-                              )
-                            )
-                          ],
+                              item.achievedTime == null
+                                  ? const SizedBox()
+                                  : Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10.0),
+                                      child: AchievedTimeWidget(
+                                          checkList: widget.checkList,
+                                          item: item),
+                                    ),
+                              AchievementButtonWidget(
+                                  checkList: widget.checkList, item: item),
+                              const Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 10.0, horizontal: 20.0),
+                                  child: Divider()),
+                              Expanded(
+                                  child: StreamBuilder<QuerySnapshot>(
+                                      stream: RoomFirestore.rooms
+                                          .doc(widget.checkList.roomId)
+                                          .collection('check_lists')
+                                          .doc(widget.checkList.id)
+                                          .collection('comments')
+                                          .orderBy('created_time',
+                                              descending: false)
+                                          .where('item_id', isEqualTo: item.id)
+                                          .snapshots(),
+                                      builder: (context, commentSnapshot) {
+                                        if (!commentSnapshot.hasData) {
+                                          return const CircularProgressIndicatorWidget();
+                                        }
+                                        List<QueryDocumentSnapshot<Object?>>
+                                            commentDocs =
+                                            commentSnapshot.data!.docs;
+                                        if (commentDocs.isEmpty) {
+                                          return const Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                  vertical: 20.0),
+                                              child: Text('コメントはまだありません'));
+                                        }
+                                        return ListView.builder(
+                                            controller: scrollController,
+                                            itemCount: commentDocs.length,
+                                            itemBuilder: (context, index) {
+                                              Timestamp?
+                                                  prevCommentCreatedTime =
+                                                  getPrevCommentCreatedTime(
+                                                      index, commentDocs);
+                                              Map<String, dynamic> data =
+                                                  commentDocs[index].data()
+                                                      as Map<String, dynamic>;
+                                              Comment comment = Comment(
+                                                  id: data['id'],
+                                                  text: data['text'],
+                                                  imagePath: data['image_path'],
+                                                  itemId: data['item_id'],
+                                                  postedAccountId:
+                                                      data['posted_account_id'],
+                                                  readAccountIds:
+                                                      data['read_account_ids'],
+                                                  createdTime:
+                                                      data['created_time']);
+                                              return CommentDetailWidget(
+                                                  roomId:
+                                                      widget.checkList.roomId,
+                                                  checkListId:
+                                                      widget.checkList.id,
+                                                  comment: comment,
+                                                  prevCommentCreatedTime:
+                                                      prevCommentCreatedTime,
+                                                  isMine: comment
+                                                          .postedAccountId ==
+                                                      currentFirebaseUser.uid);
+                                            });
+                                      }))
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  TextInputWidget(
-                    item: item,
-                    checkList: widget.checkList,
-                    scrollController: scrollController,
-                  )
-                ],
-              );
-            }
-          ),
+                    TextInputWidget(
+                      item: item,
+                      checkList: widget.checkList,
+                      scrollController: scrollController,
+                    )
+                  ],
+                );
+              }),
         ),
-          bottomNavigationBar: const AdBannerWidget(),
+        bottomNavigationBar: const AdBannerWidget(),
       ),
     );
   }
