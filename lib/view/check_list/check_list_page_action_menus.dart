70f355ddf504c2f6f2536a1e1b4112af87093982
jsl_takumi_akiba <takumi_akiba@a.jsl.co.jp>
2024-04-15 06:15:45 +0900

    右上のメニューボタンのpaddingを戻す

--- lib/view/check_list/check_list_page_action_menus.dart
+++ lib/view/check_list/check_list_page_action_menus.dart
@@ -23,7 +23,7 @@ class _CheckListPageActionMenusState extends State<CheckListPageActionMenus> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: PopupMenuButton<CheckListPopupMenuItem>(
         onSelected: (CheckListPopupMenuItem value) {
           switch (value) {
